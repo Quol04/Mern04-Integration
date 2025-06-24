11cafb4fc44f6470b9c0a37b63901b7e8c55d7a2
quol <kuolamol77@gmail.com>
2025-06-24 20:44:25 +0300

    added an auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,2 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+exports.protect= (req, res, next )=> {
+    const auth= req.headers.autharization;
+     if(!auth )  return res.status(404).json({message:""}) 
+
+}
